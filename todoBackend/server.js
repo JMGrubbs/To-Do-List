6b134487c37c4073501b6794c1acf647946baa44
root <root@ip-172-26-0-201.us-east-2.compute.internal>
2023-08-02 14:25:25 +0000

    api updates

--- todoBackend/server.js
+++ todoBackend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fs = require('fs');
 const toml = require('toml');
 const app = express();
-const port = 3001;
+const port = 3100;
 
 app.use(cors());
 app.use(express.json());
@@ -23,7 +23,12 @@ const pool = mysql.createPool({
 });
 
 // Get all todos
-app.get('/todos', async (req, res) => {
+app.get('/api/todos', async (req, res) => {
+  // Check if the 'x-api-key' header is present in the request
+  if (req.headers['x-api-key'] !== config['todoInfo']["API_KEY"]) {
+    return res.status(401).json({ error: 'API key is missing in the request header' });
+  }
+
   try {
     const [rows] = await pool.query('SELECT * FROM todos');
     res.json(rows);
@@ -34,48 +39,67 @@ app.get('/todos', async (req, res) => {
 });
 
 // Add a new todo
-// app.post('/todos', async (req, res) => {
-//   const { title } = req.body;
-
-//   if (!title) {
-//     return res.status(400).json({ error: 'Title is required' });
-//   }
-
-//   try {
-//     await pool.query('INSERT INTO todos (title) VALUES (?)', [title]);
-//     res.sendStatus(201);
-//   } catch (error) {
-//     console.error('Error adding todo:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+app.post('/api/todos', async (req, res) => {
+  // Check if the 'x-api-key' header is present in the request
+  if (req.headers['x-api-key'] !== config['todoInfo']['API_KEY']) {
+    return res.status(401).json({ error: 'API key is missing or invalid in the request header' });
+  }
+  
+  const { title } = req.body;
+  
+  if (!title) {
+    return res.status(400).json({ error: 'Title is required' });
+  }
+  console.log(title)
+  try {
+    // Insert the new todo into the database
+    const result = await pool.query('INSERT INTO todos (title) VALUES (?)', [title]);
+    // Get the auto-generated ID of the newly inserted row
+    const newTodoId = result[0].insertId;
+    // Fetch the newly created todo from the database using the ID
+    const [newTodo] = await pool.query('SELECT * FROM todos WHERE id = ?', [newTodoId]);
+    // Respond with the newly created todo in the response
+    res.status(201).json(newTodo[0]);
+  } catch (error) {
+    console.error('Error adding todo:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // // Update the completion status of a todo
-// app.patch('/todos/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const { completed } = req.body;
-//   console.log(req)
-//   try {
-//     await pool.query('UPDATE todos SET completed = ? WHERE id = ?', [completed, id]);
-//     res.sendStatus(200);
-//   } catch (error) {
-//     console.error('Error updating todo:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+app.patch('/api/todos/:id', async (req, res) => {
+  // Check if the 'x-api-key' header is present in the request
+  if (req.headers['x-api-key'] !== config['todoInfo']["API_KEY"]) {
+    return res.status(401).json({ error: 'API key is missing in the request header' });
+  }
+  const { id } = req.params;
+  const { completed } = req.body;
+  console.log(req)
+  try {
+    await pool.query('UPDATE todos SET completed = ? WHERE id = ?', [completed, id]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error updating todo:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // // Delete a todo
-// app.delete('/todos/delete/:id', async (req, res) => {
-//   const { id } = req.params;
+app.delete('/api/todos/delete/:id', async (req, res) => {
+  // Check if the 'x-api-key' header is present in the request
+  if (req.headers['x-api-key'] !== config['todoInfo']["API_KEY"]) {
+    return res.status(401).json({ error: 'API key is missing in the request header' });
+  }
+  const { id } = req.params;
 
-//   try {
-//     await pool.query('DELETE FROM todos WHERE id = ?', [id]);
-//     res.sendStatus(200);
-//   } catch (error) {
-//     console.error('Error deleting todo:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+  try {
+    await pool.query('DELETE FROM todos WHERE id = ?', [id]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
